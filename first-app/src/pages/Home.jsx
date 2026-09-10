c181c1537f7c698b4c4dbf354c00eae938b0adc6
Sujira Ploysumlee <ploysumlee_s@silpakorn.edu>
2026-09-10 05:25:50 +0000

    new data add state

--- first-app/src/pages/Home.jsx
+++ first-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { movies } from "../data/data";
 import MovieCard from "../components/MovieCards";
 import MovieGallery from "../components/MovieGallery";
